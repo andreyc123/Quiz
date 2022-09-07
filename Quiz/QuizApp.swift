7e53f41bf994c42dcb774b77da19f42b021990a3
Andrey Chernyshenko <a.chernyshenko@mobidev.biz>
2022-09-07 15:50:22 +0300

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Andrey on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
